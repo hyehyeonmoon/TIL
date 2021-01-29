b4fea4122ebde338bf8324eadab993dcd4684f44
Hyehyeon Moon <55529617+angelfox4@users.noreply.github.com>
2021-01-29 20:41:32 +0900

    Add files via upload

---
+++ ch03_word2vec.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Simple CBOW
+
+# In[ ]:
+
+
+# coding: utf-8
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul, SoftmaxWithLoss
+
+
+class SimpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        # 계층 생성
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        # 모든 가중치와 기울기를 리스트에 모은다.
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h0 = self.in_layer0.forward(contexts[:, 0])
+        h1 = self.in_layer1.forward(contexts[:, 1])
+        h = (h0 + h1) * 0.5
+        score = self.out_layer.forward(h)
+        loss = self.loss_layer.forward(score, target)
+        return loss
+
+    def backward(self, dout=1):
+        ds = self.loss_layer.backward(dout)
+        da = self.out_layer.backward(ds)
+        da *= 0.5
+        self.in_layer1.backward(da)
+        self.in_layer0.backward(da)
+        return None
+
+
+# ## Skip-gram
+
+# In[ ]:
+
+
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul, SoftmaxWithLoss
+
+
+class SimpleSkipGram:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        # 계층 생성
+        self.in_layer = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer1 = SoftmaxWithLoss()
+        self.loss_layer2 = SoftmaxWithLoss()
+
+        # 모든 가중치와 기울기를 리스트에 모은다.
+        layers = [self.in_layer, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h = self.in_layer.forward(target)
+        s = self.out_layer.forward(h)
+        l1 = self.loss_layer1.forward(s, contexts[:, 0])
+        l2 = self.loss_layer2.forward(s, contexts[:, 1])
+        loss = l1 + l2
+        return loss
+
+    def backward(self, dout=1):
+        dl1 = self.loss_layer1.backward(dout)
+        dl2 = self.loss_layer2.backward(dout)
+        ds = dl1 + dl2
+        dh = self.out_layer.backward(ds)
+        self.in_layer.backward(dh)
+        return None
+
+
+# ## train
+
+# In[ ]:
+
+
+import sys
+sys.path.append('..')  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+from common.trainer import Trainer
+from common.optimizer import Adam
+from simple_cbow import SimpleCBOW
+from common.util import preprocess, create_contexts_target, convert_one_hot
+
+
+window_size = 1
+hidden_size = 5
+batch_size = 3
+max_epoch = 1000
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+
+vocab_size = len(word_to_id)
+contexts, target = create_contexts_target(corpus, window_size)
+target = convert_one_hot(target, vocab_size)
+contexts = convert_one_hot(contexts, vocab_size)
+
+model = SimpleCBOW(vocab_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.plot()
+
+word_vecs = model.word_vecs
+for word_id, word in id_to_word.items():
+    print(word, word_vecs[word_id])
+
+
+# ## trainer
+
+# In[ ]:
+
+
+import sys
+sys.path.append('..')
+import numpy
+import time
+import matplotlib.pyplot as plt
+from common.np import *  # import numpy as np
+from common.util import clip_grads
+
+
+class Trainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_list = []
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def fit(self, x, t, max_epoch=10, batch_size=32, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        max_iters = data_size // batch_size
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            # 뒤섞기
+            idx = numpy.random.permutation(numpy.arange(data_size))
+            x = x[idx]
+            t = t[idx]
+
+            for iters in range(max_iters):
+                batch_x = x[iters*batch_size:(iters+1)*batch_size]
+                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+                # 기울기 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    avg_loss = total_loss / loss_count
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    self.loss_list.append(float(avg_loss))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = numpy.arange(len(self.loss_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.loss_list, label='train')
+        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('손실')
+        plt.show()
+
+
+# In[ ]:
+
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and                      params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
+
+
+# ## Optimizer : Adam
+
+# In[ ]:
+
+
+
+class Adam:
+    '''
+    Adam (http://arxiv.org/abs/1412.6980v8)
+    '''
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = [], []
+            for param in params:
+                self.m.append(np.zeros_like(param))
+                self.v.append(np.zeros_like(param))
+        
+        self.iter += 1
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)
+
+        for i in range(len(params)):
+            self.m[i] += (1 - self.beta1) * (grads[i] - self.m[i])
+            self.v[i] += (1 - self.beta2) * (grads[i]**2 - self.v[i])
+            
+            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
+
+
+# ## MatMul layer
+
+# In[ ]:
+
+
+from common.np import *  # import numpy as np
+from common.config import GPU
+from common.functions import softmax, cross_entropy_error
+
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+
